81b2001d9dc46fbb761814684815e7690869e43f
Akshat Goyal <goyalakshat111@gmail.com>
2024-12-27 15:30:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let map = new Map();
+	for(let t of str){
+		if(map.has(t)){
+			map.set(t,map.get(t)+1)
+		}
+		else{
+			map.set(t,1)
+		}
+	}
+
+	for(let [char,count] of map){
+		if(count==1){
+			return char
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
